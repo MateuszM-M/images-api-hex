ab8e7faf772e18c9844278dbcb71913040c42524
MateuszM-M <mateuszmikulski2@gmail.com>
2022-05-27 20:24:38 +0200

    Add image format validation

--- app/serializers.py
+++ app/serializers.py
@@ -1,4 +1,6 @@
+from PIL import Image
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from .models import Photo
 
@@ -13,6 +15,15 @@ class PhotoSerializer(serializers.ModelSerializer):
         model = Photo
         fields = ['user', 'photo']
         
+    def validate_photo(self, photo):
+        img = Image.open(photo)
+        if img.format.lower() not in ['jpeg', 'jpg', 'png']:
+            raise ValidationError(
+                "Only .jpg and .png formats are allowed."
+            )
+        return super(PhotoSerializer, self).validate(photo)
+            
+
     def save(self, **kwargs):
         """
         Include default for read_only `user` field

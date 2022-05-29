8f331aebb5d7ab34e5c274a828ea60166569f8f3
MateuszM-M <mateuszmikulski2@gmail.com>
2022-05-29 20:49:22 +0200

    Fix and add docstrings to create_thumbnails

--- app/upload_handling.py
+++ app/upload_handling.py
@@ -22,30 +22,44 @@ def create_original_image(tier, upload, image_upload):
 
 def create_thumbnails(tier, upload, image_upload):
     """
-    Create thumbnails
+    Create thumbnails.
     
-    After hours, it still has bugs :(
+    Parameters
+    ----------
+    name : takes name from image_upload to name thumbnails
+    file_format : take file format from image_upload to add it to thumbnail name
+    thumbnails : get user's tier thumbnails to be able to iterate through it
+    thb_upload : creates copy of image_upload 
+                 to be able to multiple resize through loop
+    img_io : BytesIO object to save resized image in memory
+    image : image from image_upload opened in pillow
+    hegiht : states height from tier for resizing
+    width : states image width after resize, based on height
+    resized : pillow resized image
+    new_img : instantiates InMemoryUploadedFile to change it in thb_upload
     """
-    img_io = BytesIO()
-    
-    restart_image = image_upload['image']
-    thb_upload = image_upload
+    name = image_upload['image'].name.split('.')[0]
+    file_format = image_upload['image'].name.split('.')[-1]
     
     thumbnails = tier.thumbnails.all()
     
     for t in thumbnails:
-        thb_upload['image'] = restart_image
+        thb_upload = image_upload
+        img_io = BytesIO()
         image = ImagePillow.open(thb_upload['image'])
-        resized = image.resize((t.max_height, t.max_height))
+        height = t.max_height
+        width = height * image.width // image.height
+        resized = image.resize(
+            (width, height), 
+            ImagePillow.ANTIALIAS)
         resized.save(img_io, format=image.format)
         new_img = InMemoryUploadedFile(
             img_io,
             'ImageField',
-            f"{image_upload['image'].name}_thumbnail_{t.max_height}px.{image.format}",
-            f"(image/{image.format})",
+            f"{name}_thumbnail_{t.max_height}px.{file_format}",
+            image.format,
             sys.getsizeof(img_io),
             None)
-        
         thb_upload['image'] = new_img
         Image.objects.create(upload=upload,
                              label=f"thumbnail_{t.max_height}",
